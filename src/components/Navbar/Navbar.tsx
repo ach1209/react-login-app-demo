ff28ca7bb028176e9e38d5ace765c4b6e7a1c801
Anthony <ach0074@gmail.com>
2022-07-14 12:22:26 -0400

    Added conditional check for menu items

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,17 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Navbar.module.css'
 
 function Navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
   return (
     <div className={styles.navbar}>
       <span className={styles['navbar-title']}>Login React App</span>
       <nav className={styles['navbar-menu']}>
         <Link to="/" className={styles['navbar-item']}>Home</Link>
-        <Link to="/login" className={styles['navbar-item']}>Login</Link>
-        <Link to="/" className={styles['navbar-item']}>Logout</Link>
+        { !isLoggedIn && <Link to="/login" className={styles['navbar-item']}>Login</Link> }
+        { isLoggedIn && <Link to="/" className={styles['navbar-item']}>Logout</Link> }
       </nav>
     </div>
   )
