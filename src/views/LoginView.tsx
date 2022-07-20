933e214dd89c81e96050cd8ac2a2d1eff34a5813
Anthony <ach0074@gmail.com>
2022-07-19 21:32:37 -0400

    Fixed being able to view the Profile page without having to fill out login credentials

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -3,26 +3,38 @@ import { useNavigate } from "react-router-dom"
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 import AccountForm from "../components/Form/AccountForm"
 import Button from "../components/Button/Button"
+import StatusMessage from "../components/StatusMessage/StatusMessage"
 
 function LoginView() {
   const [values, setValues] = useState({
     email: '',
     password: ''
   })
+  const [message, setMessage] = useState('')
+  const [loginError, setLoginError] = useState(false)
   const auth = getAuth()
   const navigate = useNavigate()
 
   function signIntoAccount(e: React.SyntheticEvent) {
     e.preventDefault()
     signInWithEmailAndPassword(auth, values.email, values.password)
-
-    auth.onAuthStateChanged(user => {
-      if (user) {
-        navigate('/profile', { replace: true })
-      } else {
-        console.log('nope')
-      }
-    })
+      .then(() => {
+        auth.onAuthStateChanged(user => user && navigate('/profile', { replace: true }))
+      })
+      .catch(err => {
+        setLoginError(true)
+        switch(err.code) {
+          case 'auth/invalid-email':
+            setMessage('Please enter a valid email address')
+            break;
+          case 'auth/wrong-password':
+            setMessage('The password you entered was incorrect')
+            break;
+          default:
+            setMessage('The required fields cannot be left blank')
+            break; 
+        }
+      })
   }
 
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -36,6 +48,7 @@ function LoginView() {
   return (
     <>
       <h1 className="text-center">Login to Your Account</h1>
+      { loginError && <StatusMessage message={message} status="rejected" />}
       <AccountForm
         action={signIntoAccount}
         values={values}
